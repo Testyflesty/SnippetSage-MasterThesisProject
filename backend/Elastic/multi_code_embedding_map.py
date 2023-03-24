c1598a71b709444a6fd2480114f554fd607a5e60
Wilmer Zwietering <wilmer.zwietering@dwellwell.ai>
2023-03-24 14:51:10 +0100

    Add code to multi index. GPU support for encoder

--- backend/Elastic/multi_code_embedding_map.py
+++ backend/Elastic/multi_code_embedding_map.py
@@ -2,8 +2,9 @@ import pickle
 from typing import List
 from uuid import uuid4
 
-from backend.Elastic.app_escode import Encoder
+from app_escode import Encoder
 from elasticsearch import Elasticsearch
+from tqdm import tqdm
 
 
 class MultiCodeEmbeddingMap:
@@ -23,33 +24,41 @@ class MultiCodeEmbeddingMap:
             code_snippets = pickle.load(f)
 
         encoder = Encoder("hamzab/codebert_code_search")
-        for question_id, code_snippet_index in code_snippets:
-            embedding = encoder.encode(
-                code_snippets[(question_id, code_snippet_index)], 512
-            )
-            self.insert(embedding, question_id, code_snippet_index)
+        for question_id, code_snippet_index in tqdm(code_snippets):
+            code = code_snippets[(question_id, code_snippet_index)]
+            embedding = encoder.encode(code, 512)
+            self.insert(embedding[0], question_id, code_snippet_index, code)
 
     def insert(
-        self, embedding: List[float], question_id: int, code_snippet_index: int
+        self,
+        embedding: List[float],
+        question_id: int,
+        code_snippet_index: int,
+        code_snippet: str,
     ) -> None:
         # Map the embedding to the question and code snippet in ElasticSearch
         doc = {
             "embeddings": embedding,
             "question_id": question_id,
             "code_snippet_index": code_snippet_index,
+            "code_snippet": code_snippet,
         }
-        self.es_client.index(index="embedding_map", body=doc, id=str(uuid4()))
+        self.es_client.index(index="embedding_map", document=doc, id=str(uuid4()))
 
     def setup_index(self):
+        self.es_client.indices.delete(index="embedding_map", ignore=[400, 404])
         config = {
             "mappings": {
                 "properties": {
                     "embeddings": {
                         "type": "dense_vector",
                         "dims": 768,
+                        "index": True,
+                        "similarity": "cosine",
                     },
                     "question_id": {"type": "integer"},
                     "code_snippet_index": {"type": "integer"},
+                    "code_snippet": {"type": "text"},
                 },
             },
             "settings": {"number_of_shards": 2, "number_of_replicas": 1},
@@ -60,9 +69,10 @@ class MultiCodeEmbeddingMap:
             body=config,
         )
 
+
 if __name__ == "__main__":
     # Create the embedding map in ElasticSearch
     map = MultiCodeEmbeddingMap()
     map.setup_index()
-    staqd = "StackOverflow-Question-Code-Dataset/annotation_tool/data/code_solution_labeled_data/source/python_how_to_do_it_by_classifier_multiple_iid_to_code.pickle"
+    staqd = "/home/wzwietering/Chris/StackOverflow-Question-Code-Dataset/annotation_tool/data/code_solution_labeled_data/source/python_how_to_do_it_by_classifier_multiple_iid_to_code.pickle"
     map.map_from_pickle(staqd)
